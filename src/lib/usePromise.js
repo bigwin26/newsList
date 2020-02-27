d477af99cecbbe7bdab9eb4d1d59e3e3f33dcd46
kdsgood12 <kdsgood12@daum.net>
2020-02-27 17:25:27 +0900

    ignore es-lint warning

--- src/lib/usePromise.js
+++ src/lib/usePromise.js
@@ -20,6 +20,7 @@ export default function usePromise(promiseCreator, deps) {
       setLoading(false);
     };
     process();
+    // eslint-disable-next-line
   }, deps);
   return [loading, resolved, error];
 }
